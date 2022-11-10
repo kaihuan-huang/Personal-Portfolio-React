a5a050292c2c3ff2983b439a4f0ebb9ec2de735d
kaihuan Huang <huanhuan@kaihuans-MacBook-Pro.local>
2022-11-10 14:17:51 -0800

    update contact component

--- portfolio-react-kaihuan/src/App.js
+++ portfolio-react-kaihuan/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Skill from "./components/Skill"
 import Project from "./components/Project";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <About />
       <Skill />
       <Project /> 
+      <Contact />
     </div>
   );
 }
---
+++ portfolio-react-kaihuan/src/components/Contact.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+      <div name='contact' className='w-full h-screen text-gray-500'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+          <div className='max-w-[px] w-full grid grid-cols-2 gap-8'>
+            <div className='sm:text-right pb-8 pl-4'>
+              <p className='text-4xl font-bold inline text-pink-600'>
+              Contact Me!
+              </p>
+            </div>
+            </div>
+                <form className='flex flex-col max-w-[700px] w-full'>
+                <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
+                <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
+                <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea> 
+                <button  className='text-pink-600 group border-2 px-6 py-3 hover:text-[#cab0e9]'>
+                Submit!
+               </button> 
+                </form>
+        </div>
+      </div>
+    );
+  };
+
+export default Contact
