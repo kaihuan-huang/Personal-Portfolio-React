a6f82e54374bed64672f6cf7d0e29dff58fbfbbc
kaihuanhuang <huangkaihuan0216@gmail.com>
2024-02-06 12:24:28 -0800

    added new about intro

--- portfolio-react-kaihuan/src/components/About.jsx
+++ portfolio-react-kaihuan/src/components/About.jsx
@@ -7,16 +7,16 @@ const About = () => {
         <div className='max-w-[px] w-full grid grid-cols-2 gap-8'>
           <div className='sm:text-right pb-8 pl-4'>
             <p className='text-4xl font-bold inline text-pink-600'>
-            
+            Co-Founder@Real Estate Assistant AI 
             </p>
           </div>
           </div>
           <div className='max-w-[800px] w-full grid sm:grid-cols-2 gap-8 px-4'>
             <div className='sm:text-right text-4xl font-bold'>
-              <p> </p>
+              <p> SWE | AI Engineer</p>
             </div>
             <div>
-              <p></p>  
+              <p>A Full Stack Developer and AI Specialist with a passion for building digital solutions that improve everyday life. From the strategic use of React and FastAPI to the innovative application of machine learning, I create technology that is as smart as it is functional. Dive into my portfolio to explore the projects that have benefited from my dedication to excellence and my drive to push the boundaries of what's possible. </p>  
             </div>
           </div>
       </div>
